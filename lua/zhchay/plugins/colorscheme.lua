7c4c6a02789db38754048814380460cdacd038e2
ZH Chay <mon10180218@gmail.com>
2024-12-20 17:43:49 +0800

    diagnostics color scheme

--- lua/zhchay/plugins/colorscheme.lua
+++ lua/zhchay/plugins/colorscheme.lua
@@ -7,6 +7,27 @@ require("catppuccin").setup({
   },
   integrations = {
     telescope = { enabled = true },
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+        ok = { "italic" },
+      },
+      underlines = {
+        errors = { "undercurl" },
+        hints = { "undercurl" },
+        warnings = { "undercurl" },
+        information = { "undercurl" },
+        ok = { "undercurl" },
+      },
+      inlay_hints = {
+        background = true,
+      },
+    },
+    mason = true,
     gitsigns = true,
     indent_blankline = { enabled = true, scope_color = "overlay2" },
   },
