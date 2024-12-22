d63abe09611a1ffe4fc666d4e2155d023738f867
ZH Chay <mon10180218@gmail.com>
2024-12-23 00:39:13 +0800

    dim background when telescope picker open

--- lua/zhchay/plugins/telescope.lua
+++ lua/zhchay/plugins/telescope.lua
@@ -14,3 +14,50 @@ map("n", "<leader>u", builtin.live_grep, { desc = "Telescope live grep" })
 map("n", "<leader>i", builtin.current_buffer_fuzzy_find, { desc = "Telescope find in buffer" })
 map("n", "<leader>?", builtin.help_tags, { desc = "Telescope help tags" })
 map("n", "<leader>\\", builtin.builtin, { desc = "Telescope all pickers" })
+
+-- Dim background when picker is open
+-- CONFIG
+local blend = 50
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "TelescopePrompt",
+  callback = function(ctx)
+    local backdropName = "TelescopeBackdrop"
+    local telescopeBufnr = ctx.buf
+
+    -- `Telescope` does not set a zindex, so it uses the default value
+    -- of `nvim_open_win`, which is 50: https://neovim.io/doc/user/api.html#nvim_open_win()
+    local telescopeZindex = 50
+
+    local backdropBufnr = vim.api.nvim_create_buf(false, true)
+    local winnr = vim.api.nvim_open_win(backdropBufnr, false, {
+      relative = "editor",
+      row = 0,
+      col = 0,
+      width = vim.o.columns,
+      height = vim.o.lines - 2,
+      focusable = false,
+      style = "minimal",
+      zindex = telescopeZindex - 1, -- ensure it's below the reference window
+    })
+
+    vim.api.nvim_set_hl(0, backdropName, { bg = "#000000", default = true })
+    vim.wo[winnr].winhighlight = "Normal:" .. backdropName
+    vim.wo[winnr].winblend = blend
+    vim.bo[backdropBufnr].buftype = "nofile"
+
+    -- close backdrop when the reference buffer is closed
+    vim.api.nvim_create_autocmd({ "WinClosed", "BufLeave" }, {
+      once = true,
+      buffer = telescopeBufnr,
+      callback = function()
+        if vim.api.nvim_win_is_valid(winnr) then
+          vim.api.nvim_win_close(winnr, true)
+        end
+        if vim.api.nvim_buf_is_valid(backdropBufnr) then
+          vim.api.nvim_buf_delete(backdropBufnr, { force = true })
+        end
+      end,
+    })
+  end,
+})
