0932648bda79e7cba3efa451a1c00c4075d320a6
ZH Chay <mon10180218@gmail.com>
2024-12-22 23:57:16 +0800

    fix git not initialised error

--- lua/zhchay/plugins/telescope.lua
+++ lua/zhchay/plugins/telescope.lua
@@ -4,7 +4,11 @@ local builtin = require("telescope.builtin")
 
 map("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
 map("n", "<leader>o", builtin.oldfiles, { desc = "Telescope find old files" })
-map("n", "<leader>g", builtin.git_status, { desc = "Telescope git status" })
+map("n", "<leader>g", function()
+  if not pcall(builtin.git_status) then
+    vim.notify("The current directory is not a git repository.")
+  end
+end, { desc = "Telescope git status" })
 map("n", "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
 map("n", "<leader>u", builtin.live_grep, { desc = "Telescope live grep" })
 map("n", "<leader>i", builtin.current_buffer_fuzzy_find, { desc = "Telescope find in buffer" })
