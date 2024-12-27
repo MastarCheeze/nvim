0fa1461b0df21c538e73c5c85b1f2f96b27e8276
ZH Chay <mon10180218@gmail.com>
2024-12-27 13:12:31 +0800

    don't save alpha as session

--- lua/zhchay/plugins/autosession.lua
+++ lua/zhchay/plugins/autosession.lua
@@ -1,6 +1,6 @@
 require("auto-session").setup({
   auto_restore = false,
-  bypass_save_filetypes = {},
+  bypass_save_filetypes = { "alpha" },
 })
 
 vim.keymap.set("n", "<leader>'r", "<cmd>SessionRestore<cr>", { desc = "Restore previous session" })
