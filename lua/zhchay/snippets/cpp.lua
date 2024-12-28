36904bbecb155f3bdbf62ae345a91af94ccd5430
MastarCheeze <mon10180218@gmail.com>
2024-12-28 17:12:36 +0800

    create cp files usercommand

--- lua/zhchay/snippets/cpp.lua
+++ lua/zhchay/snippets/cpp.lua
@@ -5,20 +5,35 @@ local t = ls.text_node
 local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 
-local cpTemplatePath = vim.fs.normalize("~/.config/nvim/lua/zhchay/snippets/template.cpp")
+local cp_template_path = vim.fs.normalize("~/.config/nvim/lua/zhchay/snippets/template.cpp")
+local cp_snippet = nil
 
-if vim.fn.filereadable(cpTemplatePath) == 0 then
-  vim.notify("CP template " .. cpTemplatePath .. " does not exist.")
+if vim.fn.filereadable(cp_template_path) == 0 then
+  vim.notify("CP template " .. cp_template_path .. " does not exist.")
 else
-  local f = assert(io.open(cpTemplatePath, "rb"))
-  local template = f:read("*all")
+  local f = assert(io.open(cp_template_path, "rb"))
+  local text = f:read("*all")
   f:close()
 
-  template = template:gsub("{", "{{")
-  template = template:gsub("}", "}}")
-  template = template:gsub("//$0", "{}")
+  local snip_text = text
+  snip_text = snip_text:gsub("{", "{{")
+  snip_text = snip_text:gsub("}", "}}")
+  snip_text = snip_text:gsub("//$0", "{}")
 
-  ls.add_snippets("cpp", {
-    s("template", fmt(template, { i(0) })),
-  })
+  cp_snippet = s("template", fmt(snip_text, { i(0) }))
+  ls.add_snippets("cpp", { cp_snippet })
 end
+
+-- Create new cp folder with main.cpp and data, open them side-by-side
+vim.api.nvim_create_user_command("CPNew", function(opts)
+  local dir = opts.args
+  vim.fn.mkdir(dir, "p")
+  vim.fn.writefile({}, dir .. "/main.cpp")
+  vim.fn.writefile({}, dir .. "/data")
+
+  vim.cmd("edit " .. dir .. "/main.cpp | only")
+  vim.cmd("vnew | edit " .. dir .. "/data")
+  vim.cmd("vertical resize 44")
+  vim.cmd("wincmd h")
+  ls.snip_expand(cp_snippet)
+end, { nargs = 1 })
