fcec1a94145b7bb248de48e66fb8f96875c1af13
MastarCheeze <mon10180218@gmail.com>
2024-12-30 02:30:28 +0800

    improve keymaps

--- lua/zhchay/keymaps.lua
+++ lua/zhchay/keymaps.lua
@@ -1,6 +1,9 @@
 local map = vim.keymap.set
 vim.g.mapleader = " "
 
+-- Write buffer
+map("n", "<leader>w", "<cmd>w<cr>", { desc = "Write buffer" })
+
 -- File explorer
 map("n", "<leader>e", ":e .<cr>", { desc = "Show file explorer" })
 
@@ -13,20 +16,27 @@ map("n", "<C-w>d", "<C-w>c", { desc = "Close window" }) -- <C-w><C-c> can't be r
 map("n", "<C-w><C-d>", "<C-w>c", { desc = "Close window" })
 
 -- Tab navigation
-map("n", "<C-,>", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
-map("n", "<C-.>", "<cmd>tabnext<cr>", { desc = "Next tab" })
+map("n", "<C-t>t", "<cmd>tabnew<cr>", { desc = "New tab" })
+map("n", "<C-t><C-t>", "<cmd>tabnew<cr>", { desc = "New tab" })
+map("n", "<C-t>d", "<cmd>tabclose<cr>", { desc = "Close tab" })
+map("n", "<C-t><C-d>", "<cmd>tabclose<cr>", { desc = "Close tab" })
+map("n", "<C-]>", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
+map("n", "<C-[>", "<cmd>tabnext<cr>", { desc = "Next tab" })
 
 -- Custom text-editing actions
 map({ "n", "v" }, "<leader>d", '"_d', { noremap = true, desc = "Delete to void register" })
-map("n", "<leader>vo", "o<esc>o", { desc = "Insert two newlines below cursor" })
-map("n", "<leader>vO", "O<esc>O", { desc = "Insert two newlines above cursor" })
-map("n", "<leader>vp", "O<esc>jo<esc>k", { desc = "Pad current line with newlines above and below" })
+map("n", "<leader>vo", "o<esc>o", { desc = "Insert two newlines below" })
+map("n", "<leader>vO", "O<esc>O", { desc = "Insert two newlines above" })
+map("n", "<leader>vp", "O<esc>jo<esc>k", { desc = "Pad with newlines above and below" })
 map("n", "<leader>va", "ggVG", { desc = "Select entire buffer" })
+map("n", "<leader>vr", ":%s/", { desc = "Replace text" })
 
 -- Settings
 map("n", "<leader>'n", "<cmd>set relativenumber!<cr>", { desc = "Toggle relative line number" })
-map("n", "<leader>'x", "<cmd>w<cr><cmd>source %<cr>", { desc = "Execute the current file" })
-map("n", "<leader>'X", "<cmd>.lua<cr>", { desc = "Execute the current line" })
+-- Sourcing files
+map("n", "<leader><leader>x", "<cmd>w<cr><cmd>source %<cr>", { desc = "Execute the current file" })
+map("n", "<leader><leader>X", "<cmd>.lua<cr>", { desc = "Execute the current line" })
+map("v", "<leader><leader>X", "<cmd>lua<cr>", { desc = "Execute the current selection" })
 
 -- QOL changes
 map("n", "<C-d>", "<C-d>zz", { noremap = true }) -- center cursor on screen when moving with <C-u> and <C-d>
