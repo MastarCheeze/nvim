b74526206e2a0697a71fc899d53c9bd9ce50fb00
MastarCheeze <mon10180218@gmail.com>
2025-01-26 02:33:14 +0800

    change oil keymaps

--- lua/zhchay/plugins/explorer.lua
+++ lua/zhchay/plugins/explorer.lua
@@ -6,16 +6,24 @@ MiniIcons.mock_nvim_web_devicons()
 require("oil").setup({
   delete_to_trash = true,
   view_options = {
-    show_hidden = true,
     is_always_hidden = function(name, _)
       local always_hidden = { ".DS_Store", "__pycache__" }
       return vim.tbl_contains(always_hidden, name)
     end,
   },
+  use_default_keymaps = false,
   keymaps = {
-    ["<C-h>"] = false,
-    ["<C-l>"] = false,
-    ["<C-k>"] = false,
-    ["<C-j>"] = false,
+    ["<CR>"] = "actions.select",
+    ["<ESC>"] = { "actions.close", mode = "n" },
+    ["<BS>"] = { "actions.parent", mode = "n" },
+    ["-"] = { "actions.open_cwd", mode = "n" },
+    ["`"] = { "actions.cd", mode = "n" },
+    ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+    ["g."] = { "actions.toggle_hidden", mode = "n" },
+    ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    ["gs"] = { "actions.change_sort", mode = "n" },
+    ["gr"] = "actions.refresh",
+    ["gx"] = "actions.open_external",
+    ["g?"] = { "actions.show_help", mode = "n" },
   },
 })
