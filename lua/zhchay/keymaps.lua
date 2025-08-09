00d4785794b557f5b38cf6246624fd8a6a6581cf
MastarCheeze <mon10180218@gmail.com>
2025-08-09 12:35:27 +0800

    reenable centering, disable newline keybinds

--- lua/zhchay/keymaps.lua
+++ lua/zhchay/keymaps.lua
@@ -28,9 +28,9 @@ map({ "n", "i", "v" }, "<C-t><C-n>", "<cmd>tabnext<cr>", { desc = "Next tab" })
 
 -- Custom text-editing actions
 map({ "n", "v" }, "<leader>d", '"_d', { noremap = true, desc = "Delete to void register" })
-map("n", "<leader>vo", "o<esc>o<esc>", { desc = "Insert two newlines below" })
-map("n", "<leader>vO", "O<esc>O<esc>", { desc = "Insert two newlines above" })
-map("n", "<leader>vp", "O<esc>jo<esc>k", { desc = "Pad with newlines above and below" })
+-- map("n", "<leader>vo", "o<esc>o<esc>", { desc = "Insert two newlines below" })
+-- map("n", "<leader>vO", "O<esc>O<esc>", { desc = "Insert two newlines above" })
+-- map("n", "<leader>vp", "O<esc>jo<esc>k", { desc = "Pad with newlines above and below" })
 map("n", "<leader>va", "ggVG", { desc = "Select entire buffer" })
 map("n", "<leader>vr", ":%s/", { desc = "Replace text" })
 
@@ -40,8 +40,8 @@ map("n", "<leader><leader>X", "<cmd>.lua<cr>", { desc = "Execute the current lin
 map("v", "<leader><leader>X", "<cmd>lua<cr>", { desc = "Execute the current selection" })
 
 -- QOL changes
--- map("n", "<C-d>", "<C-d>zz", { noremap = true }) -- center cursor on screen when moving with <C-u> and <C-d>
--- map("n", "<C-u>", "<C-u>zz", { noremap = true })
--- map("n", "n", "nzzzv", { noremap = true }) -- center cursor on screen when jumping between search results
--- map("n", "N", "Nzzzv", { noremap = true })
+map("n", "<C-d>", "<C-d>zz", { noremap = true }) -- center cursor on screen when moving with <C-u> and <C-d>
+map("n", "<C-u>", "<C-u>zz", { noremap = true })
+map("n", "n", "nzzzv", { noremap = true }) -- center cursor on screen when jumping between search results
+map("n", "N", "Nzzzv", { noremap = true })
 map({ "n", "v" }, "<esc>", "<cmd>noh<cr>", { desc = "Clear highlights" }) -- clear highlights from searching
