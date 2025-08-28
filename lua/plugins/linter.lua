05a4f5da98b0d252c8c3830e625747ccd612d0dd
MastarCheeze <mon10180218@gmail.com>
2025-08-28 16:15:40 +0800

    eslint no error if config file does not exist

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -9,6 +9,24 @@ lint.linters_by_ft = {
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePre", "InsertLeave" }, {
   callback = function()
+    -- check if eslint config exists
+    if vim.bo.filetype == "javascript" or vim.bo.filetype == "typescript" then
+      local eslintConfigFilenames = {
+        "eslint.config.js",
+        "eslint.config.mjs",
+        "eslint.config.cjs",
+        "eslint.config.ts",
+        "eslint.config.mts",
+        "eslint.config.cts",
+        ".eslintrc.js",
+        ".eslintrc.json",
+        ".eslintrc.cjs",
+      }
+      if vim.fs.root(0, eslintConfigFilenames) == nil then
+        return
+      end
+    end
+
     lint.try_lint()
   end,
 })
